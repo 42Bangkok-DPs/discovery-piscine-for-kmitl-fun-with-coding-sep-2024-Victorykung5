7b0b5eaba1a4d5c4fa7491cb34f80bf3f266b72d
ThanithK <victorykung5@gmail.com>
2024-09-11 21:39:17 +0700

    ex34

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    const listContainer = $('#ft_list');
+    const newTaskButton = $('#new-task');
+
+    const cookieString = document.cookie;
+    const cookieArray = cookieString.split('; ');
+    const tasksCookie = cookieArray.find(row => row.startsWith('tasks='));
+    if (tasksCookie) {
+        const tasks = JSON.parse(tasksCookie.split('=')[1]);
+        tasks.forEach(task => addTask(task));
+    }
+
+    newTaskButton.on('click', () => {
+        const task = prompt('Enter a new task:');
+        if (task && task.trim() !== '') {
+            addTask(task);
+            saveTasks();
+        }
+    });
+
+    function createTaskElement(taskText) {
+        const taskElement = $("<div></div>");
+        taskElement.addClass('task');
+        taskElement.text(taskText);
+        taskElement.on('click', () => {
+            if (confirm('Are you sure you want to delete this task?')) {
+                taskElement.remove();
+                saveTasks();
+            }
+        });
+        return taskElement;
+    }
+
+    function addTask(taskText) {
+        const taskElement = createTaskElement(taskText);
+        listContainer.prepend(taskElement); 
+    }
+
+    function saveTasks() {
+        const tasksString = JSON.stringify(listContainer.children().map(function() {
+            return $(this).text();
+        }).get().reverse());
+        document.cookie = `tasks=${tasksString};`;
+    }
+});
